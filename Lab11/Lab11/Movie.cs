71708ad1345612d34a48d8573fb02709c0704ea6
kaysvoboda <kaymcleay@gmail.com>
2021-02-13 10:33:23 -0500

    Lab 11 first commit

---
+++ Lab11/Lab11/Movie.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Lab11_02._11
+{
+    public class Movie
+    {
+        private string _title { get; set; }
+        private string _category { get; set; }
+
+        public Movie()
+        {
+
+        }
+
+        public Movie(string title, string category)
+        {
+            _title = title;
+            _category = category;
+        }
+
+        public void GetListOfMovies(string userinput)
+        {
+            var list = InitializeList();
+            list.Sort((m1, m2) => m1._title.CompareTo(m2._title));
+            foreach (var movie in list)
+            {
+                if ((movie._category == userinput) == true)
+                {
+                    Console.WriteLine(movie._title);
+                }
+            }
+        }
+
+        private List<Movie> InitializeList()
+        {
+            var list = new List<Movie>
+            {
+                new Movie("Spirited Away", "animated"),
+                new Movie("Frozen", "animated"),
+                new Movie("Friday the 13th", "horror"),
+                new Movie("Django: Unchained", "drama"),
+                new Movie("Saw", "horror"),
+                new Movie("Star Wars", "sci fi"),
+                new Movie("Star Trek", "sci fi"),
+                new Movie("Fight Club", "drama"),
+                new Movie("Jurrasic Park", "sci fi"),
+                new Movie("Se7en", "drama"),
+                new Movie("Annabelle", "horror")
+            };
+
+            return list;
+        }
+
+    }
+}
---
+++ Lab11/Lab11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab11_02._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var movie = new Movie();
+            Console.WriteLine("Welcome to the Movie List Application! There are 10 movies in the list.");
+
+            do
+            {
+                Console.WriteLine("What category are you interested in? Your category choices are: animated, drama, sci fi, or horror: ");
+                var userInput = Console.ReadLine();
+
+                if (userInput == "animated" || userInput == "drama" || userInput == "sci fi" || userInput == "horror")
+                {
+                    movie.GetListOfMovies(userInput);
+                }
+                else
+                {
+                    Console.WriteLine("Sorry! That category is currently unavailable. Please pick a valid category.");
+                }
+
+            } while (ContinueInput() == "y");
+
+            Console.WriteLine("Thanks for stopping by! Have a great day!");
+
+        }
+        public static string ContinueInput()
+        {
+            Console.WriteLine("Would you like to continue? (y/n): ");
+            string userContinue = Console.ReadLine();
+            return userContinue;
+        }
+    }
+}
